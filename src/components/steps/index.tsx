d44e420c9900d5bb87c1bfaaf4499dbb362cbc58
JoaoChi <148506329+JoaoChi@users.noreply.github.com>
2025-01-13 17:16:40 -0300

    Adicionado os icones ao componente de steps

--- src/components/steps/index.tsx
+++ src/components/steps/index.tsx
@@ -1,15 +1,24 @@
 import { View, Text } from "react-native";
 import { s } from "./styles";
 import { Step } from "../step";
+import { IconMapPin, IconQrcode, IconTicket } from "@tabler/icons-react-native";
 export function Steps(){
     return (
         <View style={s.container}>
             <Text style={s.title}> Veja como funciona: </Text>
-            <Step title="Encontre Estabelecimentos"
+            <Step 
+            icon={IconMapPin}
+            title="Encontre Estabelecimentos"
             description="Veja locais perto de você que são nossos parceiros!"/>
-            <Step title="Ative o Cupom com QR code"
+            
+            <Step 
+            icon={IconQrcode}
+            title="Ative o Cupom com QR code"
             description="Escaneie o código para utilizar do benefício."/>
-            <Step title="Garanta vantagens perto de você"
+
+            <Step 
+            icon={IconTicket}
+            title="Garanta vantagens perto de você"
             description="Ative cupons onde estiver, em diferentes tipos de estabelecimentos!"/>
         </View>
     )
