4541ae4c667e26bb9332b75e44d40eec6c643bca
JoaoChi <148506329+JoaoChi@users.noreply.github.com>
2025-01-13 15:46:17 -0300

    criado fontes e adicionado ao tema

---
+++ src/styles/font-family.ts
@@ -0,0 +1,6 @@
+export const fontFamily = {
+    bold: "Roboto-Bold, Arial-Bold, sans-serif",
+    medium: "Roboto-Medium, Arial-Medium, sans-serif",
+    regular: "Roboto-Regular, Arial, sans-serif",
+    semibold: "Roboto-SemiBold, Arial-SemiBold, sans-serif"
+};
