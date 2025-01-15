d1133f066524ed481390b132c00cf97b41711622
JoaoChi <148506329+JoaoChi@users.noreply.github.com>
2025-01-15 13:28:43 -0300

    Adicionado o componente de mapa mostrando inicialmente o a região da angellira

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -4,8 +4,14 @@ import { useEffect, useState } from "react";
 import { Categories, CategoriesProps } from "@/components/categories";
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
+import MapView from "react-native-maps"
 
 type MarketProps = PlaceProps
+
+const currentLocation = {
+    latitude: -27.097801673004962,
+    longitude: -52.60402094296817,
+}
 
 export default function Home() {
 
@@ -13,42 +19,51 @@ export default function Home() {
     const [category, setCategory] = useState("")
     const [markets, setMarkets] = useState<MarketProps[]>([])
 
-        async function fetchCategories(){
-            try {
-                const { data } = await api.get("/categories")
-                setCategories(data)
-                setCategory(data[0].id)
-            } catch (error) {
-                console.log(error)
-                Alert.alert("Categorias", "Não foi possível carregar as categorias.")
-            }
+    async function fetchCategories() {
+        try {
+            const { data } = await api.get("/categories")
+            setCategories(data)
+            setCategory(data[0].id)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Categorias", "Não foi possível carregar as categorias.")
         }
+    }
 
-        async function FetchMarkets(){
-            try {
-                if(!category){
-                    return 
-                }
-                const {data} = await api.get("/markets/category/" + category)
-                setMarkets(data)
-            } catch (error) {
-                console.log(error)
-                Alert.alert("Locais", "Não foi possível carregar os locais")
+    async function FetchMarkets() {
+        try {
+            if (!category) {
+                return
             }
+            const { data } = await api.get("/markets/category/" + category)
+            setMarkets(data)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Locais", "Não foi possível carregar os locais")
         }
+    }
 
-        useEffect(() => {
-            fetchCategories()
-        }, [])
-        
-        useEffect(() => {
-            FetchMarkets()
-        }, [category])
+    useEffect(() => {
+        fetchCategories()
+    }, [])
+
+    useEffect(() => {
+        FetchMarkets()
+    }, [category])
 
     return (
-        <View style={{flex: 1}}>
-            <Categories data={categories} onSelect={setCategory} selected={category}/>
-            <Places data={markets}/>
+        <View style={{ flex: 1 }}>
+            <Categories data={categories} onSelect={setCategory} selected={category} />
+            <MapView style={{ flex: 1 }}
+                initialRegion={
+                    {
+                        latitude: currentLocation.latitude,
+                        longitude: currentLocation.longitude,
+                        latitudeDelta: 0.005,
+                        longitudeDelta: 0.005,
+                    }
+                } />
+            <Places data={markets} />
         </View>
     )
 }
